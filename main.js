6d8e8c83b0afaa517dcda2911662ca003d1b0cf8
jacoposergio <djago32@gmail.com>
2022-06-20 16:03:15 +0200

    ARRAY POSTS

--- main.js
+++ main.js
@@ -12,14 +12,14 @@
 // TODO|    Milestone 2 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
 // TODO|    Milestone 3 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
 
-const postObjects = [
+const posts = [
        {
         id: 1,
         user: {
-            name: 'Phil mangione',
+            name: 'Phil Mangione',
             profilePic: 'https://unsplash.it/600/300?image=171'
         },
-        postDate: 06/25/2021,
+        postDate: '06/25/2021',
         postText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
         postImage: 'https://unsplash.it/600/300?image=171',
         likes: 80,
@@ -27,13 +27,43 @@ const postObjects = [
        {
         id: 2,
         user: {
-            name: 'Sofia perlari',
+            name: 'Sofia Perlari',
             profilePic: 'https://unsplash.it/600/300?image=171'
         },
-        postDate: 09/03/2021,
+        postDate: '09/03/2021',
         postText: 'Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
         postImage: 'https://unsplash.it/600/300?image=171',
         likes: 120,
-       }
+       },
+       {
+       id: 3,
+       user: {
+           name: 'Carlo Conti',
+           profilePic: 'https://unsplash.it/600/300?image=171'
+       },
+       postDate: '10/23/2019',
+       postText: 'Ciao ok tutto bene sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+       postImage: null,
+       likes: 40,
+      }
 ];
-console.log("post aggiunti",postObjects);
+console.log("post aggiunti",posts);    /*CONSOLE LOG*/
+
+// Parte dell'avvio pagina
+
+drawAllPosts(posts);
+
+// todo|-------------
+// todo|  FUNCTION
+// todo|-------------
+
+// ! Per ogni post lo stampo in pagina
+ // ! postsArray --> array dei post
+
+ function drawAllPosts(postsArray){
+    console.log("array dei post",postsArray);    /*CONSOLE LOG*/
+    for(let i = 0; i < postsArray.length; i ++){
+        const thisPost =  postsArray[i];  
+     console.log("legger post ciclo for",thisPost);   /*CONSOLE LOG*/
+    }
+ }
