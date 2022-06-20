398389c42d486420f4e663fd20795f046c33fee2
jacoposergio <djago32@gmail.com>
2022-06-20 16:21:04 +0200

    post in dom

--- main.js
+++ main.js
@@ -12,6 +12,7 @@
 // TODO|    Milestone 2 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
 // TODO|    Milestone 3 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
 
+// ! creo i post da stampare in pagina
 const posts = [
        {
         id: 1,
@@ -49,21 +50,60 @@ const posts = [
 ];
 console.log("post aggiunti",posts);    /*CONSOLE LOG*/
 
-// Parte dell'avvio pagina
+// ! Parte dell'avvio pagina
 
 drawAllPosts(posts);
 
-// todo|-------------
+// todo|-----------------------------------------------------------
 // todo|  FUNCTION
-// todo|-------------
+// todo|-----------------------------------------------------------
 
 // ! Per ogni post lo stampo in pagina
  // ! postsArray --> array dei post
 
  function drawAllPosts(postsArray){
+    // ! creo la costante per appendere nel dom
+    postInDom = document.querySelector('.posts-list');
+
     console.log("array dei post",postsArray);    /*CONSOLE LOG*/
     for(let i = 0; i < postsArray.length; i ++){
         const thisPost =  postsArray[i];  
      console.log("legger post ciclo for",thisPost);   /*CONSOLE LOG*/
+
+// !    creo il template 
+      const postTemplate = `
+      <div class="post">
+      <div class="post__header">
+          <div class="post-meta">                    
+              <div class="post-meta__icon">
+                  <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+              </div>
+              <div class="post-meta__data">
+                  <div class="post-meta__author">Phil Mangione</div>
+                  <div class="post-meta__time">4 mesi fa</div>
+              </div>                    
+          </div>
+      </div>
+      <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+      <div class="post__image">
+          <img src="https://unsplash.it/600/300?image=171" alt="">
+      </div>
+      <div class="post__footer">
+          <div class="likes js-likes">
+              <div class="likes__cta">
+                  <a class="like-button  js-like-button" href="#" data-postid="1">
+                      <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                      <span class="like-button__label">Mi Piace</span>
+                  </a>
+              </div>
+              <div class="likes__counter">
+                  Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+              </div>
+          </div> 
+      </div>            
+  </div>
+      `;
+      postInDom.innerHTML += postTemplate;
+
     }
  }
