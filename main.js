8c74781a80f89cd4a409fd5c86544228ba21d989
jacoposergio <djago32@gmail.com>
2022-06-20 16:52:53 +0200

    ok

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const posts = [
             profilePic: 'https://unsplash.it/600/300?image=171'
         },
         postDate: '06/25/2021',
-        postText: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        postText: 'Non è difficile ricostruire una vita. Basta essere consapevoli di avere la stessa forza che avevamo prima. E servircene a nostro favore.',
         postImage: 'https://unsplash.it/600/300?image=171',
         likes: 80,
        },
@@ -29,21 +29,21 @@ const posts = [
         id: 2,
         user: {
             name: 'Sofia Perlari',
-            profilePic: 'https://unsplash.it/600/300?image=171'
+            profilePic: 'https://unsplash.it/600/300?image=170'
         },
         postDate: '09/03/2021',
-        postText: 'Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
-        postImage: 'https://unsplash.it/600/300?image=171',
+        postText: 'Possiamo avere tutti i mezzi di comunicazione del mondo, ma niente… assolutamente niente, sostituisce lo sguardo dell’essere umano.',
+        postImage: 'https://picsum.photos/200',
         likes: 120,
        },
        {
        id: 3,
        user: {
            name: 'Carlo Conti',
-           profilePic: 'https://unsplash.it/600/300?image=171'
+           profilePic: 'https://picsum.photos/200'
        },
        postDate: '10/23/2019',
-       postText: 'Ciao ok tutto bene sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+       postText: 'Bisogna capire bene che in ogni istante generiamo noi stessi, che ci creiamo e ricreiamo in base alle cose che ci diciamo dentro di noi, alle immagini che partoriamo, alle nostre fantasie. Nel profondo c’è una scintilla vitale che crea incessantemente le nostre cellule e il nostro corpo. Se l’ostacoliamo corriamo dei seri rischi.',
        postImage: null,
        likes: 40,
       }
@@ -63,11 +63,16 @@ drawAllPosts(posts);
 
  function drawAllPosts(postsArray){
     // ! creo la costante per appendere nel dom
-    postInDom = document.querySelector('.posts-list');
+    postsContainer = document.querySelector('.posts-list');
+  
 
     console.log("array dei post",postsArray);    /*CONSOLE LOG*/
     for(let i = 0; i < postsArray.length; i ++){
         const thisPost =  postsArray[i];  
+        // ! creo variabile destrutturata per semplificare l'aggiunta nel post
+        let {id, user, postDate, postText, postImage, likes} = thisPost;
+        console.log("destrutturazione",postDate);
+
      console.log("legger post ciclo for",thisPost);   /*CONSOLE LOG*/
 
 // !    creo il template 
@@ -76,17 +81,17 @@ drawAllPosts(posts);
       <div class="post__header">
           <div class="post-meta">                    
               <div class="post-meta__icon">
-                  <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                  <img class="profile-pic" src="${user.profilePic}" alt="${user.name}">                    
               </div>
               <div class="post-meta__data">
-                  <div class="post-meta__author">Phil Mangione</div>
-                  <div class="post-meta__time">4 mesi fa</div>
+                  <div class="post-meta__author">${user.name}</div>
+                  <div class="post-meta__time">${postDate}</div>
               </div>                    
           </div>
       </div>
-      <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+      <div class="post__text">${postText}</div>
       <div class="post__image">
-          <img src="https://unsplash.it/600/300?image=171" alt="">
+          <img src="${postImage}" alt="">
       </div>
       <div class="post__footer">
           <div class="likes js-likes">
@@ -97,13 +102,13 @@ drawAllPosts(posts);
                   </a>
               </div>
               <div class="likes__counter">
-                  Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                  Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
               </div>
           </div> 
       </div>            
   </div>
       `;
-      postInDom.innerHTML += postTemplate;
+      postsContainer.innerHTML += postTemplate;
 
     }
  }
