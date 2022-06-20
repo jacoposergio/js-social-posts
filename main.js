18509380216c66881141ae5f4dcc514fb539ff08
jacoposergio <djago32@gmail.com>
2022-06-20 17:36:58 +0200

    numero incrementato

--- main.js
+++ main.js
@@ -55,6 +55,34 @@ console.log("post aggiunti",posts);    /*CONSOLE LOG*/
 drawAllPosts(posts);
 
 // todo|-----------------------------------------------------------
+// todo|  EVENT LISTENERS
+// todo|-----------------------------------------------------------
+
+const AllLikeButtons = document.querySelectorAll('.js-like-button');
+const AllLikesText = document.querySelectorAll('.js-likes-counter');
+console.log("like buttons",AllLikeButtons);
+for(let i=0; i < AllLikeButtons.length; i++) {
+        const thisLikeButtons = AllLikeButtons[i];
+        thisLikeButtons.addEventListener('click',function( event){
+    // !      evitiamo conmportamento di default del browser
+            event.preventDefault();
+         // ! ci prendiamo l'elemento html di testo
+         // !che ha il numero relativo a questo btn
+         const relatedNumberText = AllLikesText[i];  
+        console.log("RELATED NUMBER TEXT",relatedNumberText);   
+        // ! ci prendiamo il numero dentro il div
+        let relatedNumber = parseInt(relatedNumberText.innerHTML);
+        console.log("Numero likes",relatedNumber);
+        // ! lo incrementiamo di 1
+        relatedNumber++;
+        // ! lo scriviamo dentro al relatedNumberText
+        relatedNumberText.innerHTML = relatedNumber;
+        });
+}
+
+
+
+// todo|-----------------------------------------------------------
 // todo|  FUNCTION
 // todo|-----------------------------------------------------------
 
@@ -68,45 +96,45 @@ drawAllPosts(posts);
 
     console.log("array dei post",postsArray);    /*CONSOLE LOG*/
     for(let i = 0; i < postsArray.length; i ++){
-        const thisPost =  postsArray[i];  
-        // ! creo variabile destrutturata per semplificare l'aggiunta nel post
-        let {id, user, postDate, postText, postImage, likes} = thisPost;
-        console.log("destrutturazione",postDate);
+            const thisPost =  postsArray[i];  
+            // ! creo variabile destrutturata per semplificare l'aggiunta nel post
+            let {id, user, postDate, postText, postImage, likes} = thisPost;
+            console.log("destrutturazione",postDate);
 
-     console.log("legger post ciclo for",thisPost);   /*CONSOLE LOG*/
+        console.log("legger post ciclo for",thisPost);   /*CONSOLE LOG*/
 
-// !    creo il template 
-      const postTemplate = `
-      <div class="post">
-      <div class="post__header">
-          <div class="post-meta">                    
-              <div class="post-meta__icon">
-                  <img class="profile-pic" src="${user.profilePic}" alt="${user.name}">                    
-              </div>
-              <div class="post-meta__data">
-                  <div class="post-meta__author">${user.name}</div>
-                  <div class="post-meta__time">${postDate}</div>
-              </div>                    
-          </div>
-      </div>
-      <div class="post__text">${postText}</div>
-      <div class="post__image">
-          <img src="${postImage}" alt="">
-      </div>
-      <div class="post__footer">
-          <div class="likes js-likes">
-              <div class="likes__cta">
-                  <a class="like-button  js-like-button" href="#" data-postid="1">
-                      <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                      <span class="like-button__label">Mi Piace</span>
-                  </a>
-              </div>
-              <div class="likes__counter">
-                  Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
-              </div>
-          </div> 
-      </div>            
-  </div>
+    // !    creo il template 
+        const postTemplate = `
+        <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${user.profilePic}" alt="${user.name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${user.name}</div>
+                    <div class="post-meta__time">${postDate}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${postText}</div>
+        <div class="post__image">
+            <img src="${postImage}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
       `;
       postsContainer.innerHTML += postTemplate;
 
